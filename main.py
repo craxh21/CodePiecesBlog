4f5d3d4a16703d33c71e75d10c3c84930d25cf1f
craxh21 <mbcraxh21@gmail.com>
2024-08-21 01:18:35 +0530

    All the blog files setup & all the links for static folder files updated with jinja templating & new layout file made and other files setupped acc to it by using template inheritence & routing of the pages done in the main.py app file

--- main.py
+++ main.py
@@ -2,11 +2,23 @@
 from flask import Flask, render_template 
 app = Flask(__name__)
 
-
-@app.route("/")  
+@app.route("/" )
 def home():
     return render_template('index.html')
 
-app.run(debug= True)  
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+
+@app.route("/contact")   #get req ... used to fetch the index.html
+def contact():
+    return render_template('contact.html')
+
+@app.route("/post")
+def post():
+    return render_template('post.html')
+
+app.run(debug= True) 
 
 
